b670df05ccd047681323273597e85ea28871cd8b
sakaichihiro1209 <sakaichihi@gmail.com>
2025-09-12 17:14:46 +0900

    b

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, make_response, request
 import json
-
 from cookie_service import cookie
 
 app = Flask(__name__)
 
 @app.route('/resp')
 def resp():
+
+    return render_template('regist_group.html')
+
+@app.route('/regist-group-post', methods=['POST'])
+def regist_group():
+    group_name=request.form['group']
     cookie_service=cookie()
-    responce = cookie_service.set_group("G")
+    responce = cookie_service.set_group(group_name)
     return responce
 
 @app.route('/view')
